8e7e8a7adafd1e72562186f0a895aa806bf110ee
Edwin Magabe <edyma50@yahoo.com>
2020-10-14 11:58:42 +0300

    added jquery

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 import {MDCRipple} from '@material/ripple';
+const $ = require( "jquery" );
 
 const btn = document.querySelector('#learn');
 const buttonRipple = new MDCRipple(btn);
 
-btn.addEventListener('click', (event) => {
-    alert('good to go');
+$(btn).on('click',(event) => {
+    alert('good jquery. go');
 });
