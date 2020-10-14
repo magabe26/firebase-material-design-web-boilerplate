68ca5d16f32a4ff57029355ddcd32ffe19782035
Edwin Magabe <edyma50@yahoo.com>
2020-10-14 13:19:10 +0300

    improved using defer firebase link

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,14 @@ import {MDCRipple} from '@material/ripple';
 const btn = document.querySelector('#learn');
 const buttonRipple = new MDCRipple(btn);
 
-$(btn).on('click',(event) => {
-    alert('good jquery. go');
+$(btn).on('click', (event) => {
+    if(typeof  firebase === "undefined"){
+        alert('error -undefied');
+    }
+    firebase.firestore().collection('people').add(
+        {
+            name: 'chura',
+            age: 45
+        }
+    ).then((ref) => alert("added" + ref.id)).catch((err) => alert(err));
 });
